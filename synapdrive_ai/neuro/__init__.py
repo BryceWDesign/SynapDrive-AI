717d4761af66ea5e6e5627c16f75f734133f5cdc
Bryce Lovell <40742705+BryceWDesign@users.noreply.github.com>
2026-05-08 14:49:28 -0700

    Refactor import statements for task planner

--- synapdrive_ai/neuro/__init__.py
+++ synapdrive_ai/neuro/__init__.py
@@ -1,7 +1,13 @@
 from synapdrive_ai.neuro.band_analyzer import BANDS, BandPowerAnalyzer, BandPowerResult
 from synapdrive_ai.neuro.eeg_loader import EEGLoader, EEGRecording
 from synapdrive_ai.neuro.session_analyzer import EpochResult, SessionAnalyzer, SessionReport
-from synapdrive_ai.neuro.task_planner import ExecutorBridge, PlanTrace, StepTrace, TaskPlan, TaskStep
+from synapdrive_ai.neuro.task_planner import (
+    ExecutorBridge,
+    PlanTrace,
+    StepTrace,
+    TaskPlan,
+    TaskStep,
+)
 
 __all__ = [
     "BANDS",
